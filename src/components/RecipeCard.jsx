464e664f194760d115f404df404d16f4fbbc3e52
David-Sang96 <luainawl@gmail.com>
2024-07-25 21:55:36 +0800

    added modal

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -4,18 +4,23 @@ import { formatISO9075 } from "date-fns";
 import { AiFillEdit } from "react-icons/ai";
 import { RiDeleteBin5Fill } from "react-icons/ri";
 
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import ConfirmModal from "./ConfirmModal";
 import IngredientCard from "./IngredientCard";
 
 const RecipeCard = ({ recipe, filterRecipes }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const { title, description, ingredients, _id } = recipe;
   const apiURL = import.meta.env.VITE_API_URL;
 
-  const handleDelete = async (id) => {
+  const handleDelete = async () => {
     try {
-      const res = await axios.delete(`${apiURL}/${id}`);
-      if (res.status >= 200 || res.status < 300) {
-        filterRecipes(id);
+      const res = await axios.delete(`${apiURL}/${_id}`);
+      if (res.status >= 200 && res.status < 300) {
+        filterRecipes(_id);
+        setIsOpen(false);
       }
     } catch (error) {
       console.log(error);
@@ -23,34 +28,41 @@ const RecipeCard = ({ recipe, filterRecipes }) => {
   };
 
   return (
-    <div className="space-y-3 rounded-2xl bg-white p-5">
-      <div className="flex items-center justify-between">
-        <h3 className="text-xl font-bold text-orange">{title}</h3>
-        <div className="flex items-center gap-4">
-          <Link to={`/recipes/edit/${_id}`}>
-            <AiFillEdit
+    <>
+      <div className="space-y-3 rounded-2xl bg-white p-5">
+        <div className="flex items-center justify-between">
+          <h3 className="text-xl font-bold text-orange">{title}</h3>
+          <div className="flex items-center gap-4">
+            <Link to={`/recipes/edit/${_id}`}>
+              <AiFillEdit
+                className="cursor-pointer text-2xl text-orange"
+                title="edit"
+              />
+            </Link>
+            <RiDeleteBin5Fill
               className="cursor-pointer text-2xl text-orange"
-              title="edit"
+              title="delete"
+              onClick={() => setIsOpen((prev) => !prev)}
             />
-          </Link>
-          <RiDeleteBin5Fill
-            className="cursor-pointer text-2xl text-orange"
-            title="delete"
-            onClick={() => handleDelete(_id)}
-          />
+          </div>
         </div>
+        <p>Description</p>
+        <p>{description}</p>
+        <div className="flex space-x-1">
+          <span>Ingredients -</span>
+          <IngredientCard ingredients={ingredients} deleteAble={false} />
+        </div>
+        <p className="text-gray-500">
+          <span> Published at -</span>
+          {formatISO9075(recipe.createdAt, { representation: "date" })}
+        </p>
       </div>
-      <p>Description</p>
-      <p>{description}</p>
-      <div className="flex space-x-1">
-        <span>Ingredients -</span>
-        <IngredientCard ingredients={ingredients} deleteAble={false} />
-      </div>
-      <p className="text-gray-500">
-        <span> Published at -</span>
-        {formatISO9075(recipe.createdAt, { representation: "date" })}
-      </p>
-    </div>
+      {isOpen && (
+        <div>
+          <ConfirmModal setIsOpen={setIsOpen} handleDelete={handleDelete} />
+        </div>
+      )}
+    </>
   );
 };
 
